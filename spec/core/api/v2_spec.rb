158018f73749570119d8c98e10ed4a12bdc51f98
Rohan Likhite <rlikhite@gmail.com>
2026-08-10 15:28:55 -0400

    WIP

--- spec/core/api/v2_spec.rb
+++ spec/core/api/v2_spec.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
 RSpec.describe Bls::Core::V2 do
   describe "#base_url" do
     context "when the environment is production" do
       it "returns the correct url" do
         api = Bls::Core::V2.new(environment: "production")
-=======
-RSpec.describe IntelligentFoods::V2 do
-  describe "#base_url" do
-    context "when the environment is production" do
-      it "returns the correct url" do
-        api = IntelligentFoods::V2.new(environment: "production")
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
 
         result = api.base_url
 
@@ -22,11 +14,7 @@ RSpec.describe IntelligentFoods::V2 do
 
     context "when the environment is not production" do
       it "returns the correct url" do
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
         api = Bls::Core::V2.new(environment: "staging")
-=======
-        api = IntelligentFoods::V2.new(environment: "staging")
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
 
         result = api.base_url
 
@@ -37,11 +25,7 @@ RSpec.describe IntelligentFoods::V2 do
 
   describe "#client" do
     it "returns a api client" do
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
       api = Bls::Core::V2.new
-=======
-      api = IntelligentFoods::V2.new
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
       build_stubbed_authenticator(api)
 
       result = api.client
@@ -52,28 +36,16 @@ RSpec.describe IntelligentFoods::V2 do
 
   describe "#authenticate" do
     it "builds a authenticator" do
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
       api = Bls::Core::V2.new
-=======
-      api = IntelligentFoods::V2.new
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
       build_stubbed_authenticator(api)
 
       api.authenticate!
 
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
       expect(Bls::Core::V2::Authenticator).to have_received(:build).once
     end
 
     it "authenticates" do
       api = Bls::Core::V2.new
-=======
-      expect(IntelligentFoods::V2::Authenticator).to have_received(:build).once
-    end
-
-    it "authenticates" do
-      api = IntelligentFoods::V2.new
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
       authenticator = build_stubbed_authenticator(api)
 
       api.authenticate!
@@ -83,11 +55,7 @@ RSpec.describe IntelligentFoods::V2 do
 
     context "when the authentication is present" do
       it "is authenticated" do
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
         api = Bls::Core::V2.new
-=======
-        api = IntelligentFoods::V2.new
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
         build_stubbed_authenticator(api)
 
         api.authenticate!
@@ -99,13 +67,8 @@ RSpec.describe IntelligentFoods::V2 do
 
   describe "reset authentication" do
     it "removes the present authentication" do
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
-      auth = Bls::Core::Authorization::Basic.new(token: "1234")
-      api = Bls::Core::V2.new(authentication: auth)
-=======
       auth = IntelligentFoods::Authorization::Basic.new(token: "1234")
       api = IntelligentFoods::V2.new(authentication: auth)
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
 
       result = api.reset_authentication
 
@@ -113,13 +76,8 @@ RSpec.describe IntelligentFoods::V2 do
     end
 
     it "is not authenticated" do
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
-      auth = Bls::Core::Authorization::Basic.new(token: "1234")
-      api = Bls::Core::V2.new(authentication: auth)
-=======
       auth = IntelligentFoods::Authorization::Basic.new(token: "1234")
       api = IntelligentFoods::V2.new(authentication: auth)
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
 
       result = api.reset_authentication
 
@@ -131,11 +89,7 @@ RSpec.describe IntelligentFoods::V2 do
     it "sets the environment" do
       config = build_config(environment: "preview")
 
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
-      result = Bls::Core::V2.build(config: config)
-=======
       result = IntelligentFoods::V2.build(config: config)
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
 
       expect(result.environment).to eq("preview")
     end
@@ -143,11 +97,7 @@ RSpec.describe IntelligentFoods::V2 do
     it "sets the username" do
       config = build_config(username: "secretUsername")
 
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
-      result = Bls::Core::V2.build(config: config)
-=======
       result = IntelligentFoods::V2.build(config: config)
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
 
       expect(result.username).to eq("secretUsername")
     end
@@ -155,11 +105,7 @@ RSpec.describe IntelligentFoods::V2 do
     it "sets the password" do
       config = build_config(password: "secretPassword")
 
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
-      result = Bls::Core::V2.build(config: config)
-=======
       result = IntelligentFoods::V2.build(config: config)
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
 
       expect(result.password).to eq("secretPassword")
     end
@@ -172,17 +118,10 @@ RSpec.describe IntelligentFoods::V2 do
   end
 
   def build_stubbed_authenticator(api)
-<<<<<<< HEAD:spec/core/api/v2_spec.rb
-    authenticator = Bls::Core::V2::Authenticator.build(api)
-    authentication = authenticator.authentication
-    client = Bls::Core::ApiClient.new(authentication: authentication)
-    allow(Bls::Core::V2::Authenticator).to receive(:build).
-=======
     authenticator = IntelligentFoods::V2::Authenticator.build(api)
     authentication = authenticator.authentication
     client = IntelligentFoods::ApiClient.new(authentication: authentication)
     allow(IntelligentFoods::V2::Authenticator).to receive(:build).
->>>>>>> c99e65f (Introduce Product Resource):spec/intelligent_foods/api/v2_spec.rb
       and_return(authenticator)
     allow(authenticator).to receive(:save!).and_return(client)
     authenticator
